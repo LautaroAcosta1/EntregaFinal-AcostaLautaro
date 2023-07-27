88fda0af008b7f100fbb4a5042d8b040df3be30e
lautaro acosta <acostalauty765@gmail.com>
2023-07-27 18:00:56 -0300

    Se agrega Storage

--- funciones.js
+++ funciones.js
@@ -1,14 +1,3 @@
-// ------------------------------ simulador de base de datos ------------------------------
-
-let listaUsuarios = [
-    {usuario: "Lautaro01", clave: "12345678", email: "lauty.acosta212@gmail.com"},
-    {usuario: "pepe2002", clave: "p1p2002", email: "pepeortega302@gmail.com"},
-    {usuario: "GuilleGzlz", clave: "876345", email: "guillermo_gonzalez009@hotmail.com"},
-];
-
-
-
-
 // ------------------------------ funciones ------------------------------
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -16,7 +5,10 @@ document.addEventListener("DOMContentLoaded", function() {
     botonIniciar.addEventListener("click", iniciarSesion);
 });
 
+// Se toman los datos ingresados por la persona, se controla de que los datos pertenezcan a una cuenta creada y se 
+// inicia sesion.
 function iniciarSesion() {
+    const listaUsuarios = listaUsuariosDeLocalStorage();
     const usuarioIngresado = document.getElementById("usuarioIngresado").value;
     const claveIngresada = document.getElementById("claveIngresada").value;
 
@@ -36,15 +28,39 @@ document.addEventListener("DOMContentLoaded", function() {
     botonCrear.addEventListener("click", crearCuenta);
 });
 
+// Se toman los datos ingresados de la persona, se controla que el nombre de usuario no exista y se crea la cuenta.
 function crearCuenta() {
+    const listaUsuarios = listaUsuariosDeLocalStorage();
     const usuarioNuevoIngresado = document.getElementById("usuarioNuevoIngresado").value;
     const emailIngresado = document.getElementById("emailIngresado").value;
     const claveNuevaIngresada = document.getElementById("claveNuevaIngresada").value;
 
     let nuevoUsuario = {usuario: usuarioNuevoIngresado, clave: claveNuevaIngresada, email: emailIngresado};
 
-    listaUsuarios.push(nuevoUsuario);
-    console.log(listaUsuarios)
+    let usuarioExistente = listaUsuarios.some((user) => user.usuario === usuarioNuevoIngresado);
+
+    if (usuarioExistente) {
+        alert("El nombre de usuario ya existe. Vuelva a intentarlo.")
+    } else {
+        listaUsuarios.push(nuevoUsuario);
+        localStorage.setItem("listaUsuarios", JSON.stringify(listaUsuarios));
+        alert("Se ha registrado con exito.");
+    }
 }
+
+
+
+// Se obtienen los usuarios almacenados en el localStorage llamado "listaUsuarios".
+function listaUsuariosDeLocalStorage() {
+    const usuariosJSON = localStorage.getItem("listaUsuarios");
+    return usuariosJSON ? JSON.parse(usuariosJSON) : [];
+}
+
+
+
+
+
+
+
 
 
